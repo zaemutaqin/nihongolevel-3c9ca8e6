1e1df8e77b2d227ae1c125bba564dab76d0411a6
zaemutaqin <zae.mutaqin2306@gmail.com>
2026-06-22 10:13:57 +0700

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -148,7 +148,10 @@ function RootShell({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
-       
+       <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <meta name="google-adsense-account" content="ca-pub-3683629915373595" /> */}
+        <meta name="google-adsense-account" content="ca-pub-3683629915373595" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
