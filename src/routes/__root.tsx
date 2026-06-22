4dc43e75b47104f2cb97fb5bf3f43c27d3845e3b
zaemutaqin <zae.mutaqin2306@gmail.com>
2026-06-22 08:46:32 +0700

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -150,6 +150,19 @@ function RootShell({ children }: { children: ReactNode }) {
       <head>
         <HeadContent />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-H4TYKCR0X9"></script>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M563BQKS');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-H4TYKCR0X9');`,
